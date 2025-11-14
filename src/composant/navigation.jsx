12712c4d80249c2373fb6fa7f2296d7597a43d73
lahbouch <fatimazahraelahbouch166@gmail.com>
2025-11-14 10:14:51 +0100

    Nettoyage de main

--- src/composant/navigation.jsx
+++
@@ -1,8 +0,0 @@
-import {Component} from "react";
-function Navigation(params) {
-    return (<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium nisi voluptatibus aut, quisquam officiis quis corporis quam quo sapiente? Velit repellendus dolorem quisquam dicta illum. Voluptates facere doloremque consequatur quo.</div>);
-}
-class Navigation extends Component{
-
-}
-export default Navigation ;
